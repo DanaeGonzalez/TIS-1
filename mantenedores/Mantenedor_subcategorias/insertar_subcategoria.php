a172b3f8d55ea3e936c91fd836c823c367c55919
Javier23x <112022131+Javier23x@users.noreply.github.com>
2024-10-29 18:39:52 -0300

    Actualizacion
    Camilo me pide que le mande esta version con el mantenedor de subcategorias

---
+++ mantenedores/Mantenedor_subcategorias/insertar_subcategoria.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include '../conexion.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nombre_subcategoria = $_POST['nombre_subcategoria'];
+    $id_categoria = $_POST['id_categoria'];  //Clave foránea de la categoría
+
+    $sql = "INSERT INTO subcategoria (nombre_subcategoria, id_categoria) VALUES ('$nombre_subcategoria', '$id_categoria')";
+
+    if ($conn->query($sql) === TRUE) {
+        $_SESSION['mensaje'] = "Subcategoría creada exitosamente";
+    } else {
+        $_SESSION['mensaje'] = "Error al crear subcategoría: " . $conn->error;
+    }
+
+    header('Location: mostrar_subcategoria.php');
+    exit();
+}
+?>
