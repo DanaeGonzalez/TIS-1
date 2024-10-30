2e285f13fc8a4a134cebcbe60c7f45a6f54b34ad
MaicolRM88 <112022155+MaicolRM88@users.noreply.github.com>
2024-10-30 00:24:37 -0300

    Delete tis_usuario directory

--- tis_usuario/mantenedores/Mantenedor_producto/editar_producto.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include '../conexion.php';
-    if (isset($_GET['id'])) {
-      $id = $_GET['id'];
-      $sql = "SELECT * FROM producto WHERE id_producto = $id";
-      $result = $conn->query($sql);
-      $row = $result->fetch_assoc();
-  }
-?>
--- tis_usuario/mantenedores/categoria/editar_categoria.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include '../conexion.php';
-    if (isset($_GET['id'])) {
-        $id = $_GET['id'];
-        $sql = "SELECT * FROM categoria WHERE id_categoria = $id";
-        $result = $conn->query($sql);
-        $row = $result->fetch_assoc();
-    }
-?>
