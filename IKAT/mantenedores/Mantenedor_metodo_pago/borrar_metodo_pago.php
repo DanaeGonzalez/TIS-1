a977e379a418073a10d4f8635fb4fd5eeb84a724
MaicolRM88 <mramirezm@ing.ucsc.cl>
2024-10-30 14:58:58 -0300

    Corrección de conflictos

--- IKAT/mantenedores/Mantenedor_metodo_pago/borrar_metodo_pago.php
+++ IKAT/mantenedores/Mantenedor_metodo_pago/borrar_metodo_pago.php
@@ -11,11 +11,7 @@ include '../../config/conexion.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,opsz,wght@0,6..72,200..800;1,6..72,200..800&display=swap" rel="stylesheet">
-<<<<<<< Updated upstream
   <link rel="stylesheet" href="..\..\assets\css\styles.css">
-=======
-<link rel="stylesheet" href="../../assets/css/styles.css">
->>>>>>> Stashed changes
 </head>
 <body>
     <!-- Header/Navbar -->
@@ -25,13 +21,9 @@ include '../../config/conexion.php';
                     data-bs-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle sidebar">
                 <span class="navbar-toggler-icon"></span>
             </button>
-<<<<<<< Updated upstream
                         <a href="../../views/menu_rol/menu_adm.php">
                 <img width="180px" height="auto" src="../../assets/Images/ikat.png" alt="">
             </a>
-=======
-            <img width="180px" height="auto" src="../../assets/images/ikat.png" alt="">
->>>>>>> Stashed changes
 
             <button class="navbar-toggler border" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
                     aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
--- IKAT/mantenedores/Mantenedor_reseña/actualizar_resenia.php
+++ IKAT/mantenedores/Mantenedor_reseña/actualizar_resenia.php
@@ -10,11 +10,7 @@ include '../../config/conexion.php';
     <title>Mantenedor de Reseñas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<<<<<<< Updated upstream
   <link rel="stylesheet" href="..\..\assets\css\styles.css">
-=======
-<link rel="stylesheet" href="../../assets/css/styles.css">
->>>>>>> Stashed changes
 </head>
 
 <body>
@@ -25,13 +21,9 @@ include '../../config/conexion.php';
                     data-bs-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle sidebar">
                 <span class="navbar-toggler-icon"></span>
             </button>
-<<<<<<< Updated upstream
                         <a href="../../views/menu_rol/menu_adm.php">
                 <img width="180px" height="auto" src="../../assets/Images/ikat.png" alt="">
             </a>
-=======
-            <img width="180px" height="auto" src="../../assets/images/ikat.png" alt="">
->>>>>>> Stashed changes
 
             <button class="navbar-toggler border" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
                     aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
