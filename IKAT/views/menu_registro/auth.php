21987d24cb7ed30134ba9e6356cfd815ef832eb5
MaicolRM88 <mramirezm@ing.ucsc.cl>
2024-10-31 04:22:29 -0300

    updates registro y auth
    Se agrega en registro id_carrito asociado a un usuario, desde ahora id_carrito será incremental
    
    Auth tambien tiene un if-else ahora

--- IKAT/views/menu_registro/auth.php
+++ IKAT/views/menu_registro/auth.php
@@ -1,20 +1,22 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 
 if (!isset($_SESSION['identificador']) || !isset($_SESSION['tipo_usuario'])) {
-    header("Location: login.php");
-    exit();
+header("Location: login.php");
+exit();
 }
 
 $current_page = basename($_SERVER['PHP_SELF']);
 
 if ($current_page === 'menu_adm.php' && $_SESSION['tipo_usuario'] !== 'Admin') {
-    header("Location: ../menu_registro/menu_adm.php");
-    exit();
+header("Location: ../menu_registro/menu_adm.php");
+exit();
 }
 
-
 if ($current_page === 'menu_reg.php' && $_SESSION['tipo_usuario'] !== 'Registrado') {
-    header("Location: ../menu_registro/menu_reg.php");
-    exit();
+header("Location: ../menu_registro/menu_reg.php");
+exit();
 }
