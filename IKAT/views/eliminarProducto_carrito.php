c654a9228fa0df3af0e02dbdcbbfbec62558152d
DanaeGonzalez <danaegv2002@gmail.com>
2024-11-01 00:39:22 -0300

    Revert "Merge branch 'Maicol' into Danae"
    This reverts commit a0c061a57d32933bb96bd17d43542942d562d308, reversing
    changes made to 943d7291af5d922bc566f33a702e894508af3a03.

--- IKAT/views/eliminarProducto_carrito.php
+++ IKAT/views/eliminarProducto_carrito.php
@@ -1,8 +1,5 @@
 <?php
 include_once '../config/conexion.php';
-include 'menu_registro/auth.php';
-
-session_start(); // Inicia o reanuda la sesión
 
 if (isset($_POST['product_ids'])) {
     // Decodifica la cadena JSON a un array
@@ -10,21 +7,15 @@ if (isset($_POST['product_ids'])) {
 
     // Verifica si se obtuvo un array y no un valor nulo
     if (is_array($product_ids) && count($product_ids) > 0) {
-
         // Prepara la consulta para eliminar los productos del carrito
         $placeholders = implode(',', array_fill(0, count($product_ids), '?'));
         $sql = "DELETE FROM carrito_producto WHERE id_producto IN ($placeholders) AND id_carrito = ?";
-
         $stmt = $conn->prepare($sql);
 
         // Combina los IDs de productos con el ID del carrito
         $params = array_merge($product_ids, [$_SESSION['id_carrito']]);
-        $types = str_repeat('i', count($product_ids)) . 'i';
+        $stmt->bind_param(str_repeat('i', count($product_ids)) . 'i', ...$params); // Bind de todos los IDs
 
-        // Vincula los parámetros
-        $stmt->bind_param($types, ...$params); // Bind de todos los IDs
-
-        // Ejecuta y verifica la eliminación
         if ($stmt->execute()) {
             echo "Productos eliminados exitosamente.";
         } else {
