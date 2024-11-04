855b8ac84fc709304b85b29c666340e3402992e3
Javier23x <112022131+Javier23x@users.noreply.github.com>
2024-11-03 23:31:51 -0300

    Update catalogo.php

--- IKAT/views/catalogo.php
+++ IKAT/views/catalogo.php
@@ -10,7 +10,6 @@
         <link rel="stylesheet" href="..\assets\css\styles.css">
         <script src="../assets/js/filtros.js"></script>
         <script src="../assets/js/etiquetas.js"></script>
-        <script src="../assets/js/carrito.js"></script>
         <?php include '../assets/php/dropdowns.php'; ?>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
