b70a5c1d212192b9c87c373d5ac0b5d749a101e8
Javier23x <112022131+Javier23x@users.noreply.github.com>
2024-11-30 20:22:51 -0300

    Historial compras
    Ahora funciona la barra de búsqueda

--- IKAT/views/compras.php
+++ IKAT/views/compras.php
@@ -44,13 +44,14 @@ $id_usuario = $_SESSION['id_usuario'] ?? null; // Asigna el valor o null si no e
                             <!-- Contenedor de la barra de búsqueda -->
                             <div class="d-flex align-items-center flex-grow-1">
                                 <div class="position-relative w-100">
-                                    <input placeholder="Buscar en mis compras" type="search" class="form-control ps-5">
-                                    <svg class="position-absolute top-50 start-0 translate-middle-y ms-3" aria-hidden="true" viewBox="0 0 24 24" style="width: 1.2rem; height: 1.2rem;">
+                                    <input id="search-bar" placeholder="Buscar en mis compras" type="search" class="form-control ps-5">
+                                    <svg id="search-icon" class="position-absolute top-50 start-0 translate-middle-y ms-3" aria-hidden="true" viewBox="0 0 24 24" style="width: 1.2rem; height: 1.2rem; cursor: pointer;">
                                         <g>
                                             <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
                                         </g>
                                     </svg>
                                 </div>
+
                             </div>
                             <!-- Filtro y enlace -->
                             <div class="d-flex align-items-center gap-3">
