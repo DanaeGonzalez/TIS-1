33eee4fa14ae03d3fc764bcd2f74d7c3fb3bd253
MaicolRM88 <mramirezm@ing.ucsc.cl>
2024-12-01 02:22:00 -0300

    update postcompra
    Se modifica pantalla post compra

--- IKAT/assets/php/push_notificacion.php
+++ IKAT/assets/php/push_notificacion.php
@@ -1,8 +1,8 @@
 <?php
 $webNotificationPayload['title'] = '¡Gracias por tu compra!';
 $webNotificationPayload['body'] = 'Tu compra ha sido procesada con éxito. Haz clic aquí para ver tu pedido.';
-$webNotificationPayload['icon'] = '../../../../../assets/images/cat_blanco.jpg';
-$webNotificationPayload['url'] = '../../../../../views/compras.php';
+$webNotificationPayload['icon'] = '../assets/images/cat_blanco.jpg';
+$webNotificationPayload['url'] = 'compras.php';
 echo json_encode($webNotificationPayload);
 exit();
 ?>
