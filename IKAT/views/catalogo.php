077c14a802961af5133099c20badcc9c03693018
ccampos23 <ccamposg@ing.ucsc.cl>
2024-12-02 12:26:43 -0300

    debug

--- IKAT/views/catalogo.php
+++ IKAT/views/catalogo.php
@@ -21,6 +21,14 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,opsz,wght@0,6..72,200..800;1,6..72,200..800&display=swap" rel="stylesheet">
+        <style>
+            @media (max-width: 576px) { 
+                [id^="stars-container-"] {
+                    display: none !important;
+                }
+            }
+
+        </style>
 
     </head>
 
