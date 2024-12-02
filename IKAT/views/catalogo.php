aaef7926bcc45d63b36ac56f9ffd251ee7e811a3
Javier23x <112022131+Javier23x@users.noreply.github.com>
2024-12-02 10:04:11 -0300

    Update catalogo.php
    Vuelvo a poner eso xd

--- IKAT/views/catalogo.php
+++ IKAT/views/catalogo.php
@@ -248,9 +248,15 @@
             <?php include '../templates/footer.php'; ?>
         </div>
 
+
+
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
+
+        <script>
+            let usuarioAutenticado = <?php echo json_encode($usuarioAutenticado); ?>;
         </script>
 
 
